b52a7605ccc8acf811113dacf6707a876583a90f
santhoshgopal <santhoshgopal32@gmail.com>
2016-08-16 21:58:26 +0530

    Update arraysum.java

--- arraysum.java
+++ arraysum.java
@@ -8,16 +8,23 @@ int temp=0;
 Scanner in = new Scanner(System.in);
 String n=in.nextLine();
 int len=n.length();
+int count=0;
 char[] b=n.toCharArray();
+ArrayList<Integer> a=new ArrayList<Integer>();
 ArrayList<Character> abc=new ArrayList<Character>();
 for(int i=0;i<len;i++)
 abc.add(b[i]);
 for(char z : abc)
 {
+
 		temp=temp+Character.getNumericValue(z);
-		
+		a.add(temp);
 }
-System.out.println(temp);
+for(int z:a)
+{
+count=count+z;
+}
+System.out.println(count);
 
 		
 		if(len<=0)
